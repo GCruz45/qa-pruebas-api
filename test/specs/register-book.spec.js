afbc1e4d4e48fd89d9ee450384d869966bac5ae3
Gabriel Cruz L <gabrielcl45@gmail.com>
2022-04-04 18:39:20 -0500

    Add github actions to run tests on a PR. Finish the README file

--- test/specs/register-book.spec.js
+++ test/specs/register-book.spec.js
@@ -40,11 +40,7 @@ describe("Given I want to register a book", () => {
 });
 
 describe("Given I want to register a book and some of the fields are empty", () => {
-  // before(async () => {
-  //   // Act
-  //   response = await axios.post(`${baseUrl}/books`, book);
-  // });
-
+  // This test fails because there is a bug with the API
   it("Then it should not allow a book to be created without a name nor its author's name", async () => {
     // Arrange
     book.name = "";
@@ -69,6 +65,7 @@ describe("Given I want to register a book and some of the fields are empty", () 
     book.author = "James Joyce";
   });
 
+  // This test fails because there is a bug with the API
   it("Then it should not allow an empty book to be created", async () => {
     // Act
     response = await axios.post(`${baseUrl}/books`, {});
