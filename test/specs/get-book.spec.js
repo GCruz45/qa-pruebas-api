af2c795d000f20543fe18da81f004f58bf50e7fb
Gabriel Cruz L <gabrielcl45@gmail.com>
2022-04-04 16:05:34 -0500

    Implement get-book and implement-book tests

--- test/specs/get-book.spec.js
+++ test/specs/get-book.spec.js
@@ -1,22 +1,19 @@
-// const expect = require('chai').expect;
-// const axios = require('axios');
+const expect = require("chai").expect;
+const axios = require("axios");
 
-// const baseUrl = 'http://localhost:8080';
-// let response;
-// describe('When the user wants to list animals', () => {
-//     before(async () => {
-//         response = await axios.get(`${baseUrl}/animals`);
-//     });
-//     it('should have an OK status code', () => {
-//         expect(response.status).eql(200);
-//     });
+const baseUrl = "http://localhost:8080";
+let response;
+describe("When the user wants to list books", () => {
+  before(async () => {
+    response = await axios.get(`${baseUrl}/books`);
+  });
+  it("should have an OK status code", () => {
+    expect(response.status).eql(200);
+  });
 
-//     it("Should return animals with name, breed, gender, if it is vaccinated and vaccines", () => {
-//         const animal = response.data[0];
-//         expect(animal).to.have.property("name");
-//         expect(animal).to.have.property("breed");
-//         expect(animal).to.have.property("gender");
-//         expect(animal).to.have.property("isVaccinated");
-//         expect(animal).to.have.property("vaccines");
-//     });
-// });
+  it("Should return books with their name and their author's name", () => {
+    const books = response.data[0];
+    expect(books).to.have.property("name");
+    expect(books).to.have.property("author");
+  });
+});
--- test/specs/register-book.spec.js
+++ test/specs/register-book.spec.js
@@ -1,18 +1,16 @@
 const expect = require("chai").expect;
 const axios = require("axios");
-// const faker = require('faker').random;
-
-// name: `Mr ${faker.words(2)}`,
-const book = {
-  name: "Ulyses",
-  author: "James Joyce",
-};
 
 const baseUrl = "http://localhost:8080";
-
 let response;
 describe("When the user wants to create a book", () => {
   before(async () => {
+    // Arrange
+    const book = {
+      name: "Ulyses",
+      author: "James Joyce",
+    };
+
     // Act
     response = await axios.post(`${baseUrl}/books`, book);
   });
@@ -30,26 +28,30 @@ describe("When the user wants to create a book", () => {
     expect(createdBook.author).eql(book.author);
   });
 
-  it("should return a json as response", () => {
+  it("should return a json as a response", () => {
     const headers = response.headers;
 
     // Assert
     expect(headers["content-type"]).to.contain("application/json");
   });
-
-  it("should not allow a book to be created with no name", async () => {
+  // describe("When the user wants to create a book with empty fields", () => {
+  //   before(async () => {
+  //     // Act
+  //     response = await axios.post(`${baseUrl}/books`, book);
+  //   });
+  it("should not allow a book to be created with no name or author's name", async () => {
     // Arrange
     book.name = "";
+    book.author = "James Joyce";
 
     // Act
     response = await axios.post(`${baseUrl}/books`, book);
 
     // Assert
     expect(response.status).to.not.eql(200);
-  });
 
-  it("should not allow a book to be created with no author name", async () => {
     // Arrange
+    book.name = "Ulyses";
     book.author = "";
 
     // Act
@@ -57,6 +59,8 @@ describe("When the user wants to create a book", () => {
 
     // Assert
     expect(response.status).to.not.eql(200);
+
+    book.author = "James Joyce";
   });
 
   it("should not allow an empty book to be created", async () => {
