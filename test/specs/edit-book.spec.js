5ed7272ab4e1946b18b55900bdd8168b055f9268
Gabriel Cruz L <gabrielcl45@gmail.com>
2022-04-04 17:48:13 -0500

    Implement the delete-book test and refactor the register, edit and get-book tests

--- test/specs/edit-book.spec.js
+++ test/specs/edit-book.spec.js
@@ -5,8 +5,9 @@ const baseUrl = "http://localhost:8080";
 let createdBook;
 let updatedBook;
 let response;
-describe("Given a created book", () => {
+describe("Given I want to edit a book", () => {
   before(async () => {
+    // Arrange
     const book = {
       name: "Ulyses",
       author: "James Joyce",
@@ -15,13 +16,13 @@ describe("Given a created book", () => {
     createdBook = (await axios.post(`${baseUrl}/books`, book)).data;
   });
 
-  describe("When the user wants to update the book", () => {
+  describe("With correct values", () => {
     before(async () => {
+      // Arrange
       updatedBook = {
         name: "Ulises",
         author: "James Joice",
       };
-
       response = await axios.put(
         `${baseUrl}/books/${createdBook.name}`,
         updatedBook
@@ -29,11 +30,14 @@ describe("Given a created book", () => {
     });
 
     it("Then it should reply an OK status code", () => {
+      // Assert
       expect(response.status).eql(200);
     });
 
     it("Then it should return a book whose name and author's name is modified", async () => {
       const book = response.data;
+
+      // Assert
       expect(book.name).eql(updatedBook.name);
       expect(book.author).eql(updatedBook.author);
     });
@@ -46,7 +50,7 @@ describe("Given a created book", () => {
     });
   });
 
-  describe("When the user wants to update the book with empty values", async () => {
+  describe("With empty values", async () => {
     before(async () => {
       // Arrange
       updatedBook = {
@@ -61,13 +65,13 @@ describe("Given a created book", () => {
       );
     });
 
-    it("Then should not allow an empty book to be created", () => {
+    it("Then it should not allow an empty book to be created", () => {
       // Assert
       expect(response.status).to.not.eql(200);
     });
   });
 
-  describe("When the user wants to update the book with no name or no author's name", () => {
+  describe("With no name or no author's name", () => {
     before(async () => {
       // Arrange
       updatedBook = {
